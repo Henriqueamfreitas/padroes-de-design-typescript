cf930a329886935f8fbbb4177ee31f83d9a40976
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2026-08-18 21:17:16 -0300

    ch0303

--- Exercise Files/03_03/03_03_b.ts
+++ Exercise Files/03_03/03_03_b.ts
@@ -33,6 +33,15 @@ class TikTok {
     }
 }
 
+// we cant do the below
+// class TikTok2 extends Platform{
+//     constructor(){}
+
+//     scheduleMedia(){
+//         console.log("TikTok is ready to schedule your post.")
+//     }
+// }
+
 // Adapter 
 class TikTokAdapter extends Platform {
     tikTok : TikTok
@@ -42,7 +51,8 @@ class TikTokAdapter extends Platform {
     }
 
     postMedia() {
-       
+        this.tikTok.scheduleMedia()
+        console.log('Adapter has posted to tiktok')
     }
 }
 
@@ -52,9 +62,11 @@ function adapterClient() {
     let instagram = new Instagram()
 
     publisher.publishContentOnPlatform(instagram)
-
+    
     // Using the Adapter 
     let tikTok = new TikTok()
+    // publisher.publishContentOnPlatform(tikTok as any); // this breaks the code because 
+    // tiktok has  schedulemeedia and not postmedia
     let tikTokAdapter = new TikTokAdapter(tikTok)
 
     publisher.publishContentOnPlatform(tikTokAdapter)
